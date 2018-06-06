d3a3b012a174f1d4aa7585975b7ad5962ae3d7bc
Maximilien CRUZ <max@rockpalm.it>
2018-06-06 23:13:26 +0200

    categories

---
+++ Controller/BranchSpecial.php
@@ -0,0 +1,19 @@
+<?php
+// api/src/Controller/BookSpecial.php
+
+namespace Isics\Bundle\OpenMiamMiamBundle\Controller;
+
+use Isics\Bundle\OpenMiamMiamBundle\Entity\Branch;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BranchSpecial extends Controller
+{
+
+
+    public function __invoke(Branch $data)
+    {
+        $categories = $this->getDoctrine()->getRepository('IsicsOpenMiamMiamBundle:Category')
+        ->findLevel1WithProductsInBranch($data);
+        return $categories;
+    }
+}

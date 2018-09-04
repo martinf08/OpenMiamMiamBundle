60a9fca319c9ad2ecdebab46f9465bc4bbae58e3
Martin <martin.fontana08@gmail.com>
2018-09-04 16:19:39 +0200

    Revert "Fixed missing use"
    This reverts commit 8610e9437e2af47191205ef959c852d4dd648a20.

--- Controller/Admin/Association/ProducerController.php
+++ Controller/Admin/Association/ProducerController.php
@@ -16,7 +16,6 @@ use Isics\Bundle\OpenMiamMiamBundle\Entity\AssociationHasProducer;
 use Isics\Bundle\OpenMiamMiamBundle\Form\Type\AssociationHasProducerType;
 use Isics\Bundle\OpenMiamMiamBundle\Form\Type\AssociationProducerExportTransferType;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;

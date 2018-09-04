7f5d24f91bd505628b1237427fc52f9e15aa9243
KevSimplon <nivek.internets@gmail.com>
2018-09-04 16:54:46 +0200

    CategoryType fixtures

---
+++ DataFixtures/ORM/LoadCategorytypeData.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of the OpenMiamMiam project.
+ *
+ * (c) Isics <contact@isics.fr>
+ *
+ * This source file is subject to the AGPL v3 license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Isics\Bundle\OpenMiamMiamBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Isics\Bundle\OpenMiamMiamBundle\Entity\CategoryType;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class LoadCategoryTypeData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+        $this->translator = $this->container->get('translator');
+        $this->translator->setLocale($this->container->getParameter('locale'));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        foreach (array('food', 'household', 'gardening') as $name) {
+            $type = new CategoryType();
+            $type->setName($this->translator->trans('category_type.' . $name, array(), 'fixtures'));
+
+            $manager->persist($type);
+        }
+
+        $manager->flush();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 9;
+    }
+}

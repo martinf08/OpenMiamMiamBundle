8ab2a6c3d81920512870f28ba544fe1a17712a33
KevSimplon <nivek.internets@gmail.com>
2018-09-07 16:10:08 +0200

    query test code in dashboard

--- Controller/Admin/Super/GeneralController.php
+++ Controller/Admin/Super/GeneralController.php
@@ -13,6 +13,9 @@ namespace Isics\Bundle\OpenMiamMiamBundle\Controller\Admin\Super;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+//CODE TEST, NE PAS COMMIT
+use Doctrine\ORM\QueryBuilder;
+//END CODE TEST
 
 class GeneralController extends Controller
 {
@@ -23,6 +26,25 @@ class GeneralController extends Controller
      */
     public function showDashboardAction()
     {
-        return $this->render('IsicsOpenMiamMiamBundle:Admin\Super:showDashboard.html.twig');
+        //CODE TEST, NE PAS COMMIT
+            $conn = $this->getDoctrine()->getEntityManager()->getConnection();
+
+            $sql = '
+                SELECT sales_order_id, GROUP_CONCAT(product.id) as products, MAX(category_type.id) as type
+                FROM sales_order_row
+                JOIN product ON sales_order_row.product_id = product.id
+                INNER JOIN category ON product.category_id = category.id
+                INNER JOIN category_type ON category.category_type_id = category_type.id
+                GROUP BY sales_order_row.sales_order_id, category_type.id
+                HAVING COUNT(category.category_type_id) > 1
+                ORDER BY type
+                ';
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+
+            // returns an array of arrays (i.e. a raw data set)
+            $results = $stmt->fetchAll();
+        //END CODE TEST
+        return $this->render('IsicsOpenMiamMiamBundle:Admin\Super:showDashboard.html.twig', array('results' => $results));
     }
 }

403541739eeb17d26e07a386c550d34da2cdf749
KevSimplon <nivek.internets@gmail.com>
2018-09-11 10:34:34 +0200

    command file for associated products

---
+++ Command/ListAssociatedProductsCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the OpenMiamMiam project.
+ *
+ * (c) Isics <contact@isics.fr>
+ *
+ * This source file is subject to the AGPL v3 license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Isics\Bundle\OpenMiamMiamBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ListAssociatedProductsCommand extends ContainerAwareCommand
+{
+    /**
+     * @see ContainerAwareCommand
+     */
+    protected function configure()
+    {
+        $this->setName('openmiammiam:list-associated-products')
+            ->setDescription('Fill table with the most sold product in same orders for every given products');
+    }
+
+    /**
+     * @see ContainerAwareCommand
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $stuffManager = $this->getContainer()->get('product_association_manager?');
+        $output->writeln([
+            'My Command',
+            '==========',
+            ''
+        ]);
+        $output->writeln('hallo Leute');
+    }
+}

9f775caac43354c7967d2538340b936bb8b369b0
Martin <martin.fontana08@gmail.com>
2018-09-19 09:12:51 +0200

    first step of query builder

--- Controller/CatalogController.php
+++ Controller/CatalogController.php
@@ -85,7 +85,8 @@ class CatalogController extends Controller
         $repository = $this->getDoctrine()->getRepository('IsicsOpenMiamMiamBundle:Product');
         $product = $repository->findOneByIdAndVisibleInBranch($productId, $branch);
         $matchingProducts = $repository->findMatchingProducts($product, $branch);
-
+dump($matchingProducts);
+die();
         if (null === $product) {
             throw new NotFoundHttpException('Product not found');
         }

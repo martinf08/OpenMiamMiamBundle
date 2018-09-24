8f2241d8e17147ee535c44265fbe0b3ab030e8ea
KevSimplon <nivek.internets@gmail.com>
2018-09-24 10:52:22 +0200

    alreadyInCart function

--- Resources/assets/js/front.js
+++ Resources/assets/js/front.js
@@ -265,3 +265,16 @@ OpenMiamMiam.AccordionFocus = function() {
 
     return object;
 }();
+
+OpenMiamMiam.AlreadyInCart = function() {
+
+    var object = function() {
+        $('#frequent-purchases button[type="submit"]').on('click', function (e) {
+            setTimeout(()=>{
+                $(this).parent().replaceWith('<p class="label label-info">Ce produit a déjà été ajouté à votre panier.</p>');
+            }, 1000);
+        });
+    };
+    
+    return object;
+}();

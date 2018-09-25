1f5c264ec657aeae924d14f790ae868b0b476ff9
KevSimplon <nivek.internets@gmail.com>
2018-09-25 11:25:21 +0200

    WIP updateMatchingProducts command

--- Command/UpdateMatchingProductsCommand.php
+++ Command/UpdateMatchingProductsCommand.php
@@ -15,6 +15,8 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Isics\Bundle\OpenMiamMiamBundle\Manager\ProductMatchingManager;
 
 class UpdateMatchingProductsCommand extends ContainerAwareCommand
 {
@@ -24,7 +26,7 @@ class UpdateMatchingProductsCommand extends ContainerAwareCommand
     protected function configure()
     {
         $this->setName('openmiammiam:update-matching-products')
-            ->setDescription('Fill table with the most sold product in same orders for every given products');
+             ->setDescription('Fill table with the most sold product in same orders for every given products');
     }
 
     /**
@@ -32,9 +34,21 @@ class UpdateMatchingProductsCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $repository = $this->getContainer()->get('doctrine')
-            ->getEntityManager()
-            ->getRepository("IsicsOpenMiamMiamBundle:ProductMatching")
-            ->updateMatchingProducts();
+        // $progressBar = new ProgressBar($output, 100);
+        $output->writeln('<comment>Computing matching products...</comment>');
+        // $progressBar->start();
+
+        // $i = 0;
+        // while ($i++ < 100) {
+
+            $productMatchingManager = $this->getContainer()->get('open_miam_miam.product_matching_manager');
+            $productMatchingManager->updateMatchingProducts();
+
+        //     $progressBar->advance();
+        // }
+
+        // $progressBar->finish();
+        $output->writeln('');
+        $output->writeln('<info>Task is completed.</info>');
     }
 }

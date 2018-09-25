ff19dcd8203a300f6869e09c5df32f9832fcf26b
KevSimplon <nivek.internets@gmail.com>
2018-09-25 16:05:04 +0200

    some cleaning, delete useless import

--- Entity/Repository/ProductMatchingRepository.php
+++ Entity/Repository/ProductMatchingRepository.php
@@ -11,7 +11,6 @@
 
 namespace Isics\Bundle\OpenMiamMiamBundle\Entity\Repository;
 use Doctrine\ORM\EntityRepository;
-use Isics\Bundle\OpenMiamMiamBundle\Entity\ProductMatching;
 
 class ProductMatchingRepository extends EntityRepository
 {
@@ -22,34 +21,36 @@ class ProductMatchingRepository extends EntityRepository
      * 
      * @return array
      */
-    public function updateMatchingProducts($id) {
+    public function updateMatchingProducts($id)
+    {
         $em = $this->getEntityManager();
         $conn = $em->getConnection();
 
         $deleteQuery = 'DELETE FROM product_matches WHERE product_id = :id';
         $stmt = $conn->prepare($deleteQuery);
-        $stmt->bindParam('id',$id);
+        $stmt->bindParam('id', $id);
         $stmt->execute();
 
-        $insertQuery = 'INSERT INTO product_matches (product_id, matching_product_id, nb_common_orders)
-        SELECT sor2.product_id AS `product_id`, sor1.product_id AS matching_product_id, COUNT(sor1.id) AS nb_common_orders
-                  FROM sales_order_row AS sor1
-                  JOIN sales_order_row AS sor2 ON (sor2.sales_order_id = sor1.sales_order_id)
-                  JOIN product as p1 ON p1.id = sor1.product_id
-                  JOIN category AS cat1 ON p1.category_id = cat1.id
-                  JOIN category_type AS ctt1 ON cat1.category_type_id = ctt1.id
-                  
-                  JOIN product as p2 ON p2.id = sor2.product_id
-                  JOIN category AS cat2 ON p2.category_id = cat2.id
-                  JOIN category_type AS ctt2 ON cat2.category_type_id = ctt2.id
-                  
-                  WHERE sor1.product_id != sor2.product_id
-                  AND sor2.product_id = :id
-                  AND ctt1.id = ctt2.id
-                  GROUP BY matching_product_id, `product_id`
-                  ORDER BY `product_id`, nb_common_orders DESC';
+        $insertQuery = '
+            INSERT INTO product_matches (product_id, matching_product_id, nb_common_orders)
+            SELECT sor2.product_id AS `product_id`, sor1.product_id AS matching_product_id, COUNT(sor1.id) AS nb_common_orders
+            FROM sales_order_row AS sor1
+            JOIN sales_order_row AS sor2 ON (sor2.sales_order_id = sor1.sales_order_id)
+            JOIN product as p1 ON p1.id = sor1.product_id
+            JOIN category AS cat1 ON p1.category_id = cat1.id
+            JOIN category_type AS ctt1 ON cat1.category_type_id = ctt1.id
+            
+            JOIN product as p2 ON p2.id = sor2.product_id
+            JOIN category AS cat2 ON p2.category_id = cat2.id
+            JOIN category_type AS ctt2 ON cat2.category_type_id = ctt2.id
+            
+            WHERE sor1.product_id != sor2.product_id
+            AND sor2.product_id = :id
+            AND ctt1.id = ctt2.id
+            GROUP BY matching_product_id, `product_id`
+            ORDER BY `product_id`, nb_common_orders DESC';
         $stmt2 = $conn->prepare($insertQuery);
-        $stmt2->bindParam('id',$id );
+        $stmt2->bindParam('id', $id);
         $stmt2->execute();
     }
 }

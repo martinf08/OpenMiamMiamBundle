e8cb213f974c1964dbcf56935acb7ecb7220fb7c
Martin <martin.fontana08@gmail.com>
2018-09-26 09:59:16 +0200

    remove useless class used for command

--- Command/UpdateMatchingProductsCommand.php
+++ Command/UpdateMatchingProductsCommand.php
@@ -12,12 +12,9 @@
 namespace Isics\Bundle\OpenMiamMiamBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Helper\ProgressBar;
-
-use Isics\Bundle\OpenMiamMiamBundle\Manager\ProductMatchingManager;
 
 class UpdateMatchingProductsCommand extends ContainerAwareCommand
 {
